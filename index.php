13060ab7e2d7a11a45bb17bf8c074f386f68fd76
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-02-01 09:48:49 +0100

    kijuyhtgrf

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const CONST1 = 12;
+const CONST2 = 2;
+echo CONST1 * CONST2 ."<br>";
+
+const CONST3 = "c'est cool PHP";
+echo CONST1 * CONST2 * strlen(CONST3) . "<br>";
 
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+
+echo $_SERVER["PHP_SELF"]. "<br>";
+echo $_SERVER["REMOTE_ADDR"]. "<br>";
+echo $_SERVER["REQUEST_TIME"]. "<br>";
+
+echo basename(__DIR__) ."<br>";
+echo basename(__FILE__)."<br>";
+echo basename(__METHOD__). "<br>";
+echo basename(__NAMESPACE__);
